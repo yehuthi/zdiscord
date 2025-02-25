f16f16c6ce47082b500b53ed9c2ae6c3a770701a
yehuthi <yehuthith@gmail.com>
2025-02-25 20:16:00 +0200

    add snowflake timestamp functions

--- src/common.zig
+++ src/common.zig
@@ -1,1 +1,27 @@
+const std = @import("std");
+
 pub const Snowflake = u64;
+
+/// Gets the snowflake's timestamp in Discord epoch (2015).
+pub fn snowflake_timestamp_raw_ms(snowflake: Snowflake) u64 {
+	return snowflake >> 22;
+}
+
+/// Gets the snowflake's timestamp in UNIX epoch (1970).
+pub fn snowflake_timestamp_ms(snowflake: Snowflake) u64 {
+	return snowflake_timestamp_raw_ms(snowflake) + 1420070400000;
+}
+
+test "snowflake timestamp raw" {
+	try std.testing.expectEqual(
+		41944705796,
+		snowflake_timestamp_raw_ms(175928847299117063),
+	);
+}
+
+test "snowflake timestamp" {
+	try std.testing.expectEqual(
+		1462015105796,
+		snowflake_timestamp_ms(175928847299117063),
+	);
+}
